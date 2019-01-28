6a7eaa8010e938e42bfb6605b7f8b4fa9a45d6de
EddyArellanes <you@example.com>
2019-01-28 13:52:08 -0600

    Added Calendar Test made in VueJs

--- js/kommunicate.js
+++ js/kommunicate.js
@@ -1,7 +1,11 @@
 (function(d, m){
-      var kommunicateSettings = {"appId":"1345df364f99cad05ff86f2831af0630d","conversationTitle":"Eddy AB"};
+      var kommunicateSettings = {"appId":"1345df364f99cad05ff86f2831af0630d","conversationTitle":"Eddy Arellanes"};      
       var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
       s.src = "https://api.kommunicate.io/v2/kommunicate.app";
-      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);     
       window.kommunicate = m; m._globals = kommunicateSettings;
 })(document, window.kommunicate || {});
+
+var cssChanges = ".mck-msg-right .mck-msg-box{background-color: black!important;}";
+window.Kommunicate.customizeWidgetCss(cssChanges);
+
